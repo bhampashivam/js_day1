d52c42470b83d0add18472ed7203e0be7a40e519
Shivam Parashar <95082128+bhampashivam@users.noreply.github.com>
2023-05-09 00:08:18 +0530

    Add files via upload

---
+++ day1assignment.js
@@ -0,0 +1,16 @@
+document.write("</br>");
+//printing name
+let myname = "shivam"
+document.write("My Name is "+"<i>" +myname+"</i>")
+
+document.write("</br>");
+
+    //printing city
+   let city = "New Delhi"
+	document.write("My Birth City is "+ city);
+
+  document.write("</br></br>Printing through JS")
+  var hobbies=["Cricket", "Music","Travelling"]
+  document.write("Here are my hobbies: </br>")
+  document.write("<ul><li>"+ hobbies[1]+ "</li>")
+  document.write("<li>"+ hobbies[1]+ "</li><li>"+ hobbies[2] +"</li></ul>");
